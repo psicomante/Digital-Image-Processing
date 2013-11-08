d8acdefd25dfc16d5a5886239522d8dab3d5d864
Psicomante <psicomante@gmail.com>
2013-11-08 16:55:55 +0100

    lut output refactoring

--- l1_lut/lut_negative.m
+++ l1_lut/lut_negative.m
@@ -1,5 +1,5 @@
 % Negative Lut reimplementation without intlut
-function O = lut_negative(I)
+function [O, lut] = lut_negative(I)
     lut = 256:-1:1;
     O = uint8(reshape(lut(I(:) + 1) - 1, size(I,1), size(I,2)));
 end  
--- l1_lut/lut_pseudocolor.m
+++ l1_lut/lut_pseudocolor.m
@@ -1,13 +1,8 @@
-function [G, lutG] = lut_pseudocolor(I)
+function [G, lutG, lutR, lutB] = lut_pseudocolor(I)
     block = uint8(255/6);
     lutG = [linspace(0,255,block), linspace(255,255,block*2), linspace(255,0,block), linspace(0,0,block*2)];
     lutR = circshift(lutG, [0, 43*4]);
     lutB = circshift(lutG, [0, 43*2]);
-    
-    hold on
-    plot(lutG, 'g');
-    plot(lutR, 'r');
-    plot(lutB, 'b');
     
     G(:,:,1) = uint8(reshape(lutR(I(:) + 1) - 1, size(I,1), size(I,2)));
     G(:,:,2) = uint8(reshape(lutG(I(:) + 1) - 1, size(I,1), size(I,2)));
