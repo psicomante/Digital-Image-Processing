ad7866aa4c12c7a161092195e1d463d83973f283
Psicomante <psicomante@gmail.com>
2013-11-24 18:44:51 +0100

    test imaddgaussiannoise

---
+++ tests/test_imaddgaussiannoise.m
@@ -0,0 +1,14 @@
+I = imread('images/goldhill.pgm');
+
+figure('name', 'Gaussian Noise');
+
+n = [0.1, 0.5, 1, 5, 10, 50];
+
+c = length(n) / 3
+if mod(length(n),3) > 0
+    c = uint8(c)+1;
+end
+
+for i = 1:length(n);
+    psiplot(imaddgaussiannoise(I, n(i)), [double(c), 3, i], sprintf('Intensity: %f', n(i)), 'show');
+end
