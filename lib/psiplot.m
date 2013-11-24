3a19cb9f7f4c5d4c40b238c4148380b05459dffd
Psicomante <psicomante@gmail.com>
2013-11-24 18:44:36 +0100

    complete psiplot

--- lib/psiplot.m
+++ lib/psiplot.m
@@ -1,6 +1,32 @@
 function psiplot(M, Mpos, Mtitle, type)
 
 subplot(Mpos(1), Mpos(2), Mpos(3));
-imagesc(M);
-title(Mtitle);
 
+h = showim(M, type);
+
+set(h,'ButtonDownFcn', {@openfig, M, type, Mtitle});
+
+title(Mtitle);
+end
+
+function openfig(h, e, M, type, title)
+ figure('name', title);
+ showim(M, type)
+end
+
+function h = showim(I, type)
+
+if strcmp(type,'show')
+  h = imshow(I);
+end
+
+if strcmp(type,'sc')
+  h = imagesc(I);
+end
+
+if strcmp(type,'scbar')
+  h = imagesc(I);
+  colorbar;
+end
+
+end
