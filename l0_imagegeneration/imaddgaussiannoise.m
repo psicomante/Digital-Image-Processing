21f135442e9370a38334945c257978675920200d
Psicomante <psicomante@gmail.com>
2013-11-24 17:41:39 +0100

    update gaussian noise

--- l0_imagegeneration/imaddgaussiannoise.m
+++ l0_imagegeneration/imaddgaussiannoise.m
@@ -1,7 +1,17 @@
-function [G, noise] = imaddgaussiannoise(I, strength)
-    if nargin == 1
-        strength = 20;
-    end
+% the functions adds gaussian noise on an image I
+% throught the formula:
+% G(x,y) = I(x,y) + N(x,y)
 
-    noise = randn(size(I))*strength;
-    G = uint8(double(I) + noise);
+% I: the original image you add the noise on
+% intensity: the noise intensity. It means that the intensity have a range
+% [0, intensity]
+
+% the output image has a uint8 type.
+function [G, noise] = imaddgaussiannoise(I, intensity)
+
+if nargin == 1
+    strength = 20;
+end
+
+noise = randn(size(I))*strength;
+G = uint8(double(I) + noise);
