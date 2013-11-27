e4a4fed0449225e72e7b0f8a83e0f736447adc04
Psicomante <psicomante@gmail.com>
2013-11-27 22:35:28 +0100

    updating imcos: + zero frequency case
    Adding the case to have only horizontal or vertical sinusoid

--- l0_imagegeneration/imcos.m
+++ l0_imagegeneration/imcos.m
@@ -3,11 +3,19 @@
 % fH: horizontal frequency
 % fV: vertical frequency
 function G = imcos(imsize, fH, fV)
-    y = 0:imsize;
 
+y = 0:imsize;
+
+sV = ones(1, imsize);
+sH = sV;
+
+if (fH > 0)
     sH = cos(2*pi*fH*y/(imsize+1));
-    sV = cos(2*pi*fV*y/(imsize+1));
-
-    % matrix product to build the image
-    G = sV'*sH;
 end
+
+if (fV > 0)
+    sV = cos(2*pi*fV*y/(imsize+1));
+end
+
+% matrix product to build the image
+G = sV'*sH;
